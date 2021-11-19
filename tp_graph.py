eb8db10b4d09582cab6ba35eea676e551b23eb88
JuanPatricioMarshall <juan.pmarshall@hotmail.com>
2021-11-19 12:04:23 -0300

    Nueva funcion de peso simplemente multiplicando el peso por la cantidad de incompatibilidades

--- tp_graph.py
+++ tp_graph.py
@@ -1,15 +1,14 @@
 import sys
 import networkx as nx
 from networkx import max_weight_clique
-from networkx.algorithms.approximation import maximum_independent_set
 
 INPUT_FILENAME = "input.txt"
 OUTPUT_FILENAME = "output.txt"
 
 sys.stdout = open(OUTPUT_FILENAME, 'w')
 
-def calcular_peso(cant_compatibilidades, cant_prendas, peso_prenda):
-    return peso_prenda * (cant_prendas / cant_compatibilidades) * 100
+def calcular_peso(cant_incompatibilidades, cant_prendas, peso_prenda):
+    return (peso_prenda) * (cant_incompatibilidades)
 
 with open(INPUT_FILENAME) as input:
     header = input.readline().split()
@@ -65,7 +64,7 @@ with open(INPUT_FILENAME) as input:
         cant_incompatibilidades_de_prenda = len(incompatibilidades)
         cant_compatibilidades = cant_prendas - cant_incompatibilidades_de_prenda
         peso_prenda = prendas[prenda]
-        weight = calcular_peso(cant_compatibilidades, cant_prendas, peso_prenda)
+        weight = calcular_peso(cant_incompatibilidades_de_prenda, cant_prendas, peso_prenda)
         nx.set_node_attributes(compatibilidades, {prenda: {'weight': int(weight)}})
 
 
@@ -75,25 +74,6 @@ with open(INPUT_FILENAME) as input:
         prendas_a_lavar = max_weight_clique(compatibilidades, weight='weight')[0]
         compatibilidades.remove_nodes_from(prendas_a_lavar)
         prendas_lavadas += prendas_a_lavar
-        # for prenda, prendas_incompatibles in incompatibilidades.items():
-        #     for otra_prenda in prendas_posibles:
-        #         if otra_prenda not in prendas_incompatibles and otra_prenda != prenda and prenda not in prendas_lavadas and otra_prenda not in prendas_lavadas:
-        #             peso_prenda = prendas[prenda]
-        #             # capaz en algun momento me convenga guardar el peso de las aristas
-        #             peso_otra_prenda = prendas[otra_prenda]
-        #             peso_max = max(peso_prenda, peso_otra_prenda)
-        #
-        #             weights = nx.get_node_attributes(compatibilidades, 'weight')
-        #
-        #             if weights[prenda] < peso_max:
-        #                 nx.set_node_attributes(compatibilidades,
-        #                                        {prenda: {'weight': max(peso_max - peso_prenda, peso_prenda)}})
-        #
-        #             if weights[otra_prenda] < peso_max:
-        #                 nx.set_node_attributes(compatibilidades, {
-        #                     otra_prenda: {'weight': min(peso_max - peso_otra_prenda, peso_otra_prenda)}})
-        #
-        #             compatibilidades.add_edge(prenda, otra_prenda, weight=peso_max)
         for prenda in prendas_a_lavar:
             print(f'{prenda} {lavado}')
         lavado += 1
